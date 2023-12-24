77a6a0d220da3f5eec80efdfc54749d076ae6f1d
Abdo Sharaf <bodyasharaf8@gmail.com>
2023-12-24 04:36:18 +0200

    First try

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Scene View
+    implementation("io.github.sceneview:arsceneview:0.10.0")
 }
